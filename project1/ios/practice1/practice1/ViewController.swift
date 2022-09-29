fda41bc180072c70952dce7f55b1432e4acddcae
정세진 <2016025932>
2022-09-30 01:53:12 +0900

    create project1 ios

---
+++ project1/ios/practice1/practice1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  practice1
+//
+//  Created by APPLE on 2022/09/30.
+//  Copyright © 2022 sejin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
